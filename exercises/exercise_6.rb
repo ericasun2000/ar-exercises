580fb5338eb3c9a734cae4313be476f5eeb9d7a6
Erica Sun <ericasun@seas.upenn.edu>
2020-07-21 03:28:15 +0000

    Add validation to Store and Employee models

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -15,4 +15,4 @@ puts "----------"
 @store1.employees.create(first_name: "Chloe", last_name: "Mersereau", hourly_rate: 70)
 
 @store2.employees.create(first_name: "Katelyn", last_name: "McQuillen", hourly_rate: 60)
-@store2.employees.create(first_name: "Maya", last_name: "Tatarelli", hourly_rate: 70)
+@store2.employees.create(first_name: "Maya", last_name: "Tatarelli", hourly_rate: 210)
