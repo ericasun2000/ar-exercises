ddfefc4db81d147f35fdae9135420efeb7a43cc7
Erica Sun <ericasun@seas.upenn.edu>
2020-07-21 01:07:09 +0000

    Done exercise 6

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,12 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+
+@total_revenue = Store.sum(:annual_revenue)
+@average_revenue = @total_revenue / Store.count
+
+puts "The total revenue for the entire company is $#{@total_revenue}."
+
+puts "The average revenue for all stores is $#{@average_revenue}."
+
+puts "#{Store.where("annual_revenue >= ?", 1000000).count} stores are generating $1M or more in annual sales."
--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,10 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Erica", last_name: "Sun", hourly_rate: 90)
+@store1.employees.create(first_name: "Chloe", last_name: "Mersereau", hourly_rate: 70)
+
+@store2.employees.create(first_name: "Katelyn", last_name: "McQuillen", hourly_rate: 60)
+@store2.employees.create(first_name: "Maya", last_name: "Tatarelli", hourly_rate: 70)
